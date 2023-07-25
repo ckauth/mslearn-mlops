6849aa7efd3b164f09d6f168906023a6d253a981
Christian Kauth <christian.kauth@live.com>
2023-07-25 21:22:38 +0200

    Created Azure ML job. Local: 'python .\src\model\train.py --training_data experimentation\data\diabetes-dev.csv --reg_rate 0.2', Cloud: Cloud: 'az ml job create --file job.yml --resource-group azure-ml --workspace-name azure-ml'

--- src/model/train.py
+++ src/model/train.py
@@ -26,12 +26,15 @@ def main(args):
 
 
 def get_csvs_df(path):
+    return pd.read_csv(path)
+    """
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+    """
 
 
 # add function to split data
