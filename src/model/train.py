4ef258cafbea7e717da23e41c9a5fdd2bdac16b4
ckauth <christian.kauth@live.com>
2023-07-28 08:23:40 +0200

    fixed linting errors in src

--- src/model/train.py
+++ src/model/train.py
@@ -1,14 +1,12 @@
 # Import libraries
 
 import argparse
-import glob
-import os
-
 import mlflow
 
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
 
 # define functions
 def main(args):
@@ -39,7 +37,9 @@ def get_csvs_df(path):
 
 # add function to split data
 def split_data(df):
-    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values
+    X = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+            'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+            'Age']].values
     y = df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
@@ -64,6 +64,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
